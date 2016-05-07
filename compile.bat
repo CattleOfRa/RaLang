0e33294a2f3312a660905d591d47de1197b35b40
Daniel Pacheco <dan08ish@gmail.com>
2016-05-08 00:51:03 +0100

    Compiling windows programs - Screenshots

--- compile.bat
+++ compile.bat
@@ -4,6 +4,6 @@ call lein.bat run %file%
 java -jar ../jasmin/jasmin.jar output-2.ra
 set /p class="Class name: "
 java %class%
-del output-2.ra
-del output-1.ra
+call del output-2.ra
+call del output-1.ra
 pause
